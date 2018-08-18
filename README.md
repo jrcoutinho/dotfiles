920ef25eb996f1d9362876738811a67428cb0bb8
Julio Ribeiro <julio@ribeiro.com>
2018-08-17 22:59:55 -0300

    Updated install script with symlinks and shell setup

--- README.md
+++ README.md
@@ -2,3 +2,9 @@
 My personal dotfiles
 
 Based on https://github.com/driesvints/dotfiles
+
+## Installation
+
+1. `git clone` repo to your root ~/
+2. `cd ~/dotfiles`
+3. `sh install.sh`
--- install.sh
+++ install.sh
@@ -1,8 +1,13 @@
 #!/bin/sh
-
 echo "Setting up your Mac..."
 
-# Check for Homebrew and install if we don't have it
+##### Variables
+dir=~/dotfiles                    # dotfiles directory
+olddir=~/dotfiles_old             # old dotfiles backup directory
+files="zshrc vimrc"               # list of files/folders to symlink in homedir
+
+##### Check for Homebrew and install if we don't have it
+echo "Step 1/x: Install Homebrew and dependencies listed in the Brewfile"
 if test ! $(which brew); then
   /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
 fi
@@ -14,5 +19,22 @@ brew update
 brew tap homebrew/bundle
 brew bundle
 
-# Create symlinks
+##### Create symlinks
+echo "Create symlinks to the repo's dotfiles"
+
+# create dotfiles_old in homedir
+echo "Creating $olddir for backup of any existing dotfiles in ~"
+mkdir -p $olddir
+cd $dir
+
+# move any existing dotfiles in homedir to dotfiles_old directory, then create symlinks 
+for file in $files; do
+    echo "Moving any existing dotfiles from ~ to $olddir"
+    mv ~/.$file ~/dotfiles_old/
+    echo "Creating symlink to $file in home directory."
+    ln -s $dir/$file ~/.$file
+done
+
 # Set zsh as default shell
+echo "$(which zsh)" | sudo tee -a /etc/shells
+chsh -s $(which zsh)
